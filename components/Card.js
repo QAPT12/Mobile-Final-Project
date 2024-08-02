7f7608421ddb68d1b17d49797a7c69e76be8a58c
quintin.tuck <tuckquintin@hotmail.com>
2024-08-02 09:35:12 -0230

    Default image if ImageLinks are not working are not working (i.e. emulators at school cannot connect to internet)

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,22 @@
 import { Text, Image, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
 
 export default function Card({props}) {
+
+    const [imageError, setImageError] = useState(false);
+
+    const handleImageError = () => {
+        setImageError(true);
+    }
+
     return (
         <View style={styles.container}>
-            <Image source={{uri: `${ props['imageLink']}`}} style={styles.image}/>
+            {/* <Image source={{uri: `${ props['imageLink']}`}} style={styles.image}/> */}
+            <Image
+                source={imageError ? require('./../assets/placeholder.jpeg') : { uri : props['imageLink'] }} 
+                style={styles.image}
+                onError={handleImageError}
+            />
             <Text style={styles.name}>Card Name: { props['name'] }</Text>
             <Text style={styles.text}>Color: { props['color']}</Text>
             <Text style={styles.text}>Set Released: { props['releaseSet']}</Text>
@@ -15,6 +28,7 @@ const styles = StyleSheet.create ({
     container: {
         flex: 1,
         padding: 15,
+        marginTop: 50,
         alignItems: 'center',
     },
     name: {
@@ -23,8 +37,8 @@ const styles = StyleSheet.create ({
         marginBottom: 5,
     },
     image: {
-        width: 400,
-        height: 500,
+        width: 300,
+        height: 400,
         marginBottom: 5,
     },
     text: {
